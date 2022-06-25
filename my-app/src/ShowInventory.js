c0c58bded25ed75c946bf316a3eb3ab1f3bc7d71
ShruPai <shruthipai1924@gmail.com>
2022-06-25 23:31:44 +0530

    Made for form

--- my-app/src/ShowInventory.js
+++ my-app/src/ShowInventory.js
@@ -1,32 +1,41 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import InvCard from './InvCard';
-import {getDocs,collection} from "firebase/firestore";
-import {db} from "./firebase";
+import { getDocs, collection, addDoc, updateDoc, doc } from "firebase/firestore";
+import { db } from "./firebase";
+import Showform from "./form";
+import { Link, Routes, Route } from "react-router-dom"
 
 function ShowInventory() {
-  const [invRecs,setInvRecs]=useState([]);
-  const invCollectionRef=collection(db,"inventory");
+  const [newcat, setNewCat] = useState("");
+  const [newitemname, setitemname] = useState("");
+  const [newitemcost, setitemcost] = useState(0);
+  const [invRecs, setInvRecs] = useState([]);
+  const invCollectionRef = collection(db, "inventory");
 
- 
-  useEffect(()=>{
-    const getInvRecs=async()=>{
-      const data=await getDocs(invCollectionRef);
-      setInvRecs(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
-      
+
+  useEffect(() => {
+    const getInvRecs = async () => {
+      const data = await getDocs(invCollectionRef);
+      setInvRecs(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+
     };
     getInvRecs();
   })
+
+  const createRow = async () => {
+    await addDoc(invCollectionRef, { category: newcat, itemname: newitemname, itemcost: Number(newitemcost) });
+  };
 
 
   return (
 
     <div class="container px-5 py-24 mx-auto">
-      
+
       <div class="flex flex-col text-center w-full mb-20">
         <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
           My Inventory
         </h1>
-        
+
         <table class="table-auto w-full text-left whitespace-no-wrap mt-10">
           <thead>
             <tr class="grid grid-cols-3 gap-4">
@@ -43,7 +52,7 @@ function ShowInventory() {
           </thead>
         </table>
         {
-          invRecs.map((invRecItem,index)=>{
+          invRecs.map((invRecItem, index) => {
             return <InvCard
               key={index}
               name={invRecItem.itemname}
@@ -52,14 +61,16 @@ function ShowInventory() {
             ></InvCard>
           })
         }
-   
+
       </div>
-      <button type="button" class="btn " style={{backgroundColor: 'rgb(79, 90, 132)', color:'white'}}>Add Item</button>
-      
-      
+      <Link to='/form' type="button" class="btn " style={{ backgroundColor: 'rgb(79, 90, 132)', color: 'white' }}>Add Item</Link>
+      <Routes> <Route path='/form' element={<Showform />} />
+      </Routes>
+
     </div>
-    
+
   );
 }
+
 
 export default ShowInventory;
