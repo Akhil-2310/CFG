5b143a36c17d684b4a6e5f134aafe0e819ca15da
Paridhi Jain <paridhijain0201@gmail.com>
2022-06-26 04:15:25 +0530

    4:15 commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,7 +19,7 @@ function App() {
   }]
   return (
     <div className="App">
-      <ShowInventory/>
+      {/* <ShowInventory/> */}
       {/* <Display users={users}/> */}
       {/* <Header/> */}
       
