8447b8e9de67863e1903fd658f30e7655265c43d
sreejasomavarapu <sreejasomavarapu29@gmail.com>
2022-06-25 18:56:12 +0530

    Added Card List with styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,16 @@ import Home from '../src/Components/Home/Home'
 import Cart from './Components/donorpage/Cart';
 import Header from './Components/Navbar/Header';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import CardsList from './CardsList';
+
 function App() {
-  
+
   return (
 
     <div className="App">
-      <Header/>
-      <Cart/>
+      
+      <CardsList/>
     </div>
   );
 }
---
+++ my-app/src/MenuApi.js
@@ -0,0 +1,104 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "name1",
+      category: "category1",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "name2",
+      category: "category2",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "name3",
+      category: "category1",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "name4",
+      category: "category3",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "name5",
+      category: "category2",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "name6",
+      category: "category4",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "name7",
+      category: "category4",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "name8",
+      category: "category3",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "name9",
+      category: "category2",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 9,
+        image: "../images/samosa.jpg",
+        name: "name9",
+        category: "category439",
+        price: "10₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+      {
+        id: 9,
+        image: "../images/samosa.jpg",
+        name: "name9",
+        category: "abcd",
+        price: "10₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+  ];
+  
+  export default Menu;
